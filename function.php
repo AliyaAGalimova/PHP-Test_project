1419a9b4edafd4f71e43ef4fb766cfdb21932b88
Trinity <trinity@VirtualBox-11796906.localdomain>
2024-03-19 20:29:48 +0000

    tests_added

--- function.php
+++ function.php
@@ -1,99 +1,103 @@
 <?php
-// $txt = "Ca.t,";
-// $txt = "домИК";
-$text = "is 'cold' now";
-// $txt = "third-part";
 
-// собираем массив слов
-$words = preg_split("/[\s,-`]+/", $text, -1, PREG_SPLIT_NO_EMPTY);
-
-// собираем массив символов-разделителей
-$delimeters = array();
-
-for ($i = 0; $i < strlen($text); $i++) {
-    if (preg_match('/[\s-`]+/', $text[$i]) == true)
+class String
+{
+    public function strangeString(string $text): string
     {
-      $delimeters[$i] = $text[$i];
+        // собираем массив слов
+        $words = preg_split("/[\s,-`]+/", $text, -1, PREG_SPLIT_NO_EMPTY);
+
+        // собираем массив символов-разделителей
+        $delimeters = array();
+
+        for ($i = 0; $i < strlen($text); $i++) {
+            if (preg_match('/[\s-`]+/', $text[$i]) == true)
+            {
+            $delimeters[$i] = $text[$i];
+            }
+        }
+
+        // создаем массив для хранения результата
+        $new_text = array();
+
+        // работаем отдельно с каждым словом
+
+        foreach ($words as $txt) {
+
+        $array = array();
+            
+        for ($i = 0; $i < strlen($txt); $i++) {
+            if (preg_match('/[A-Z]/u', $txt[$i]) == true)
+            {
+                $array[] = $i;
+            }
+        }
+
+        $output = '';
+        $array_punct = array();
+
+        // собираем строку без знаков препинания
+        for ($i = 0; $i < strlen($txt); $i++) {
+            if(preg_match('/[[:punct:]]/', $txt[$i])==true)
+            {
+                $array_punct[$i] = $txt[$i];
+            } else {
+                $output .= $txt[$i];
+            }
+        }
+
+        // переворачиваем строку и переводим все символы в нижний регистр
+        $helper = strtolower(strrev($output)); 
+
+
+        // добавляем знаки препинания
+        
+        $result = '';
+        for ($i = 0; $i < strlen($txt); $i++) {
+            if(array_key_exists($i, $array_punct)) {
+                $result .= $array_punct[$i];
+                if(isset($helper[$i])) {
+                    $result .= $helper[$i];
+                }
+            }  else {
+                $result .= $helper[$i];
+            }
+        }
+
+        // переводим символы в верхний регистр там, где необходимо
+        
+        $txt_new = '';
+        for ($i = 0; $i < strlen($result); $i++) {
+            if(in_array($i, $array)) {
+                $txt_new .= strtoupper($result[$i]);
+            } else {
+                $txt_new .= $result[$i];
+            }
+        }
+
+        $new_text[] = $txt_new;
+        }
+
+        print_r($new_text);
+
+        $output_string = '';
+
+        for ($i = 0; $i < count($new_text); $i++) {
+            if($i == 0 && array_key_exists(0, $delimeters)) {
+                $output_string .= $delimeters[$i];
+                unset($delimeters[$i]);
+            }
+            
+            if($i == 0) {
+                $delimeters = array_values($delimeters);
+            }
+            
+            $output_string .= $new_text[$i] . $delimeters[$i];
+        }
+
+        return $output_string;
     }
+
+
 }
-
-// создаем массив для хранения результата
-$new_text = array();
-
-// работаем отдельно с каждым словом
-
-foreach ($words as $txt) {
-
-  $array = array();
-    
-  for ($i = 0; $i < strlen($txt); $i++) {
-      if (preg_match('/[A-Z]/', $txt[$i])==true)
-      {
-         $array[] = $i;
-      }
-  }
-
-  $output = '';
-  $array_punct = array();
-
-  // собираем строку без знаков препинания
-  for ($i = 0; $i < strlen($txt); $i++) {
-      if(preg_match('/[[:punct:]]/', $txt[$i])==true)
-      {
-          $array_punct[$i] = $txt[$i];
-      } else {
-          $output .= $txt[$i];
-      }
-  }
-
-  // переворачиваем строку и переводим все символы в нижний регистр
-  $helper = strtolower(strrev($output)); 
-
-
-  // добавляем знаки препинания
-  
-  $result = '';
-  for ($i = 0; $i < strlen($txt); $i++) {
-      if(array_key_exists($i, $array_punct)) {
-          $result .= $array_punct[$i];
-          if(isset($helper[$i])) {
-              $result .= $helper[$i];
-          }
-      }  else {
-          $result .= $helper[$i];
-      }
-  }
-
-  // переводим символы в верхний регистр там, где необходимо
-  
-  $txt_new = '';
-  for ($i = 0; $i < strlen($result); $i++) {
-      if(in_array($i, $array)) {
-          $txt_new .= strtoupper($result[$i]);
-      } else {
-          $txt_new .= $result[$i];
-      }
-  }
-
-  $new_text[] = $txt_new;
-}
-
-print_r($new_text);
-
-$output_string = '';
-
-for ($i = 0; $i < count($new_text); $i++) {
-      if($i == 0 && array_key_exists(0, $delimeters)) {
-          $output_string .= $delimeters[$i];
-          unset($delimeters[$i]);
-      }
-      
-      if($i == 0) {
-      	$delimeters = array_values($delimeters);
-      }
-      
-      $output_string .= $new_text[$i] . $delimeters[$i];
-  }
-
-echo $output_string;
 ?>
---
+++ tests/Unit/StringTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/../../function.php';
+
+class StringTest extends TestCase
+{
+    public function test_first_upper_1(): void
+    {
+        $string = new String();
+        $input = 'Cat';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'Tac');
+    }
+
+    public function test_first_upper_2(): void
+    {
+        $string = new String();
+        $input = 'Мышь';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'Ьшым');
+    }
+
+    public function test_upper_in_the_middle(): void
+    {
+        $string = new String();
+        $input = 'houSe';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'esuOh');
+    }
+
+    public function test_two_uppers_in_a_row(): void
+    {
+        $string = new String();
+        $input = 'домИК';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'кимОД');
+    }
+
+    public function test_two_uppers(): void
+    {
+        $string = new String();
+        $input = 'elEpHant';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'tnAhPele');
+    }
+
+    public function test_punctuation(): void
+    {
+        $string = new String();
+        $input = 'cat,';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'tac,');
+    }
+
+    public function test_punctuation_and_uppercase_letter(): void
+    {
+        $string = new String();
+        $input = 'Зима:' ;
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'Амиз:');
+    }
+
+    public function test_sentence_1(): void
+    {
+        $string = new String();
+        $input = "is 'cold' now" ;
+        $result = $string->strangeString($input);
+        $this->assertSame($result, "si 'dloc' won");
+    }
+
+    public function test_sentence_2(): void
+    {
+        $string = new String();
+        $input = 'это «Так» "просто"';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'отэ «Так» "отсорп"');
+    }
+
+    public function test_separators_1(): void
+    {
+        $string = new String();
+        $input = 'third-part';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'driht-trap');
+    }
+
+    public function test_separators_2(): void
+    {
+        $string = new String();
+        $input = 'can`t';
+        $result = $string->strangeString($input);
+        $this->assertSame($result, 'nac`t');
+    }
+}
